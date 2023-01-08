8c2d206d1a022604766e04e1673b2714b0b67a37
Jordan Ho <jordanho.work@gmail.com>
2023-01-07 16:21:24 -0800

    clean slate

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import './App.css';
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
     <div className="App">
-      <div>
-        hello
-      </div>
+      <div>hello</div>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
