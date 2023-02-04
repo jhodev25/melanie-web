4037c1dc1e0f8ce93159fe1182e19c5d18d6a200
Jordan Ho <jordanho.work@gmail.com>
2023-02-04 15:57:35 -0800

    committing old changes

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,7 +1,10 @@
+import { melHome } from 'assets/images';
+
 export const Home = () => {
   return (
     <div className="w-auto h-screen bg-beige columns-2">
       <h1 className="font-title text-9xl">Home</h1>
+      <img src={melHome} alt="cover" />
       <h1 className="font-imbdue text-9xl">Whereas</h1>
     </div>
   );
