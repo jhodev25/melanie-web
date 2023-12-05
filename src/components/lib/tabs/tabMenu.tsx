5cf98da4a39871282c8f829b0d64fb8536be54aa
Jordan Ho <jordanho.work@gmail.com>
2023-12-05 00:55:43 -0500

    finishing home page styling

--- src/components/lib/tabs/tabMenu.tsx
+++ src/components/lib/tabs/tabMenu.tsx
@@ -1,14 +1,32 @@
 import { Box, Tabs, Tab } from '@mui/material';
+import { useState } from 'react';
 
 export const TabMenu = () => {
+  const [activeProjectsMenu, setActiveProjectsMenu] = useState(0);
   return (
-    <div className="bg-black text-white">
+    <div className="bg-black h-fit w-full absolute text-white">
       <Box sx={{ borderBottom: 1, borderColor: 'grey' }}>
         <Tabs aria-label="nav bar">
-          <Tab className="text-white" label="Home" onClick={() => window.location.assign('/')} />
-          <Tab className="text-white" label="Projects" onClick={() => window.location.assign('projects')} />
-          <Tab className="text-white" label="About" onClick={() => window.location.assign('about')} />
-          <Tab className="text-white" label="Contact Me" onClick={() => window.location.assign('contact')} />
+          <Tab
+            className="text-white font-title text-2xl pl-8"
+            label="Home"
+            onClick={() => window.location.assign('/')}
+          />
+          <Tab
+            className="text-white font-title text-2xl pl-8"
+            label="Projects"
+            onClick={() => window.location.assign('projects')}
+          />
+          <Tab
+            className="text-white font-title text-2xl pl-8"
+            label="About"
+            onClick={() => window.location.assign('about')}
+          />
+          <Tab
+            className="text-white font-title text-2xl pl-8"
+            label="Contact Me"
+            onClick={() => window.location.assign('contact')}
+          />
         </Tabs>
       </Box>
     </div>
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -4,14 +4,17 @@ import { melHome } from 'assets/images';
 
 export const Home = () => {
   return (
-    <div className="w-auto h-screen bg-black text-white columns-2">
-      <img src={melHome} alt="cover" />
-      <div>
+    <div className="w-auto h-auto bg-black text-white columns-2">
+      <div className="flex h-screen items-center justify-center">
+        <img src={melHome} alt="cover" />
+      </div>
+      <div className="h-screen flex flex-col justify-center items-left relative">
         <h1 className="font-title text-9xl">Melanie Ngai</h1>
         <br />
         <h2 className="font-title text-3xl">Marketing | Event Coordination</h2>
         <h2 className="font-title text-3xl">Based In San Francisco, California</h2>
-        <Button className="bg-olive" onClick={() => window.location.assign('/contact')} variant="contained">
+        <br />
+        <Button className="bg-olive w-fit" onClick={() => window.location.assign('/contact')} variant="contained">
           Get in Touch
         </Button>
       </div>
